8b7d810de34eb5b8c1b37021cb7b72108d4d6e68
Benoît Zugmeyer <bzugmeyer@gmail.com>
2018-06-16 21:52:40 +0200

    rewrite tests

--- src/__tests__/fixtures/readme_example/input.js
+++
@@ -1,8 +0,0 @@
-// @test-export
-function appendFoo(str) {
-    return str + "foo"
-}
-
-module.exports = function (str) {
-    return appendFoo(str).toUpperCase()
-}
--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,25 +1,102 @@
-const path = require("path")
-const fs = require("fs")
-const { transformFileSync } = require("babel-core")
+const { transform } = require("babel-core")
 
-function trim(str) {
-  return str.replace(/^\s+|\s+$/, "")
+function transformCode(code) {
+  return transform(code, { plugins: ["../../.."] }).code
 }
 
-describe("strip `exports test = ...` code from your codebase", () => {
-  const fixturesDir = path.join(__dirname, "fixtures")
-  fs.readdirSync(fixturesDir).map(caseName => {
-    it(`should pass ${caseName.split("-").join(" ")}`, () => {
-      const fixtureDir = path.join(fixturesDir, caseName)
-      const actual = transformFileSync(path.join(fixtureDir, "input.js"), {
-        plugins: ["../../.."],
-      }).code
+test("simple function", () => {
+  expect(
+    transformCode(`
+// @test-export
+function foo() {}
+    `)
+  ).toMatchSnapshot()
+})
 
-      const expected = fs
-        .readFileSync(path.join(fixtureDir, "expected.js"))
-        .toString()
+test("simple variable", () => {
+  expect(
+    transformCode(`
+// @test-export
+const foo = 42;
+    `)
+  ).toMatchSnapshot()
+})
 
-      expect(trim(actual)).toBe(trim(expected))
-    })
-  })
+test("function imbrication", () => {
+  expect(
+    transformCode(`
+// @test-export
+function foo(str) {
+  console.log(str);
+  function foo() {}
+  foo();
+}
+
+// @test-export
+function bar() {
+  bar.biz = 1;
+}
+
+foo("foo");
+    `)
+  ).toMatchSnapshot()
+})
+
+test("variables usage inside functions", () => {
+  expect(
+    transformCode(`
+// @test-export
+let foo, bar;
+
+// @test-export
+let a = 42;
+
+foo = 12;
+
+bar = foo;
+biz = 12;
+
+function blah() {
+  return foo;
+}
+
+function blih() {
+  var foo;
+  return foo;
+}
+    `)
+  ).toMatchSnapshot()
+})
+
+test("previous statement trailing comment", () => {
+  expect(
+    transformCode(`
+// foo
+const Biz = {
+    bar: true,
+}
+
+// @test-export
+function format(rights) {
+    console.log('whatever')
+}
+
+module.exports = {}
+    `)
+  ).toMatchSnapshot()
+})
+
+test("readme example", () => {
+  expect(
+    transformCode(`
+// @test-export
+function appendFoo(str) {
+    return str + "foo"
+}
+
+module.exports = function (str) {
+    return appendFoo(str).toUpperCase()
+}
+    `)
+  ).toMatchSnapshot()
 })
